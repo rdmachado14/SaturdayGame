9c0abdb61e47e5511fd9905c4e91d208c167a0f1
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-08-17 09:30:15 -0400

    Tentando fazer com que uma mensagem seja exibida na tela.

--- TesteBolaMagica/TesteBolaMagica/GameScene.swift
+++ TesteBolaMagica/TesteBolaMagica/GameScene.swift
@@ -14,25 +14,9 @@ class GameScene: SKScene
     {
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
         }
-    }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
